c41837918fcabd1162214de878fc2ac5e0cfd44f
jordansbenjamin <14327@coderacademy.edu.au>
2023-04-18 21:10:08 +1000

    Added first part of comment to do's

---
+++ hangman.py
@@ -0,0 +1,5 @@
+#Step 1
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
