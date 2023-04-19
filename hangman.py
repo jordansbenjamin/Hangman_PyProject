9cba2219b121805d8d0b78f9be38cd93b2be02a0
jordansbenjamin <14327@coderacademy.edu.au>
2023-04-19 20:36:13 +1000

    Added second part of to do's comments

--- hangman.py
+++ hangman.py
@@ -3,3 +3,17 @@
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+#Step 2
+
+#TODO-1: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+
+#TODO-2: - Loop through each position in the chosen_word;
+#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+
+#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+
