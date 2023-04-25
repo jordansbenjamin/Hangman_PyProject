dd5e95761c35793fb54deb1e57c6f48eaefaf3fb
jordansbenjamin <14327@coderacademy.edu.au>
2023-04-25 21:53:13 +1000

    Added more to do's

--- hangman.py
+++ hangman.py
@@ -2,6 +2,8 @@
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
 #Step 2
@@ -16,4 +18,15 @@
 
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+
+#Step 3
+
+
+#Testing code
+
+#Create blanks
+
+
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+
 
