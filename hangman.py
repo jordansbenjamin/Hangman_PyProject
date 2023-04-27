8df42ff9afa132d37b3d6a004692af948e9efe01
jordansbenjamin <14327@coderacademy.edu.au>
2023-04-27 21:11:20 +1000

    Added more to do's

--- hangman.py
+++ hangman.py
@@ -29,4 +29,31 @@
 
 #TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
 
+#Step 4
 
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
+#Set 'lives' to equal 6.
+
+
+#Testing code
+
+
+#Create blanks
+
+
+    #Check guessed letter
+    
+
+    #TODO-2: - If guess is not a letter in the chosen_word,
+    #Then reduce 'lives' by 1. 
+    #If lives goes down to 0 then the game should stop and it should print "You lose."
+    
+
+  
+    #Join all the elements in the list and turn it into a String.
+    
+
+    #Check if user has got all letters.
+    
+
+    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
