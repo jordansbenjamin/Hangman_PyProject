5eccec1f8459b6d75dc8b637b8a23ee11b534e59
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-08 15:55:05 +1000

    Finished step 3

--- hangman.py
+++ hangman.py
@@ -10,19 +10,19 @@ print(chosen_word)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
-guess = input("Guess a letter: ")
+# guess = input("Guess a letter: ")
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
-for char in chosen_word:
-    if guess == char:
-        print("Right")
-    else:
-        print("Wrong")
+# for char in chosen_word:
+#     if guess == char:
+#         print("Right")
+#     else:
+#         print("Wrong")
 
 #Step 2
 
-#TODO-1: - Create an empty List called display.
+#TODO-4: - Create an empty List called display.
 #For each letter in the chosen_word, add a "_" to 'display'.
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 
@@ -31,28 +31,26 @@ display = []
 for char in chosen_word:
     display.append("_")
 
-#TODO-2: - Loop through each position in the chosen_word;
+#Step 3
+
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+
+while '_' in display:
+    guess = input("Guess a letter: ")
+
+
+#TODO-5: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
 
-for index, letter in enumerate(chosen_word):
-    if guess == letter:
-        display[index] = letter
+    for index, letter in enumerate(chosen_word):
+        if guess == letter:
+            display[index] = letter
 
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+#TODO-6: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
 
-print(display)
-
-#Step 3
-
-
-#Testing code
-
-#Create blanks
-
-
-#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+    print(display)
 
 #Step 4
 
