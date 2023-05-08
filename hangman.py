3b1790dc9eaa17362cff3577bbf36638538cd0b0
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-08 14:40:38 +1000

    Added word list

--- hangman.py
+++ hangman.py
@@ -1,5 +1,7 @@
 #Step 1
 
+word_list ["ardvark", "baboon", "camel"]
+
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
