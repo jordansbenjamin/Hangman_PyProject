204a277cfcc157ccafefaa20df9f63c4c5427d94
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-08 15:19:28 +1000

    Finished to do 1 step 2

--- hangman.py
+++ hangman.py
@@ -26,6 +26,13 @@ for char in chosen_word:
 #For each letter in the chosen_word, add a "_" to 'display'.
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 
+display = []
+
+for char in chosen_word:
+    display.append("_")
+
+print(display)
+
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
